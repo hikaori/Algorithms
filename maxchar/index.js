d916954dae12a7fcbbc049449e1e54c57605636f
hikaori <kaori55194@gmail.com>
2020-04-29 14:12:56 -0700

    "for (let key in object) {}" , "x = null || 1"

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,20 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const maxChars = {};
+  let max = 0;
+  let maxChar = '';
+  for (let char of str) {
+    maxChars[char] = maxChars[char] + 1 || 1;
+  }
+  for (let char in maxChars) {
+    if (maxChars[char] > max) {
+      max = maxChars[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
