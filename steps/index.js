5f98941ff6191f8b0beea915ceebdc47b4c528a8
hikaori <kaori55194@gmail.com>
2020-05-12 14:53:21 -0700

    add steps solution recursion

---
+++ steps/index.js
@@ -0,0 +1,56 @@
+// --- Directions
+// Write a function that accepts a positive number N.
+// The function should console log a step shape
+// with N levels using the # character.  Make sure the
+// step has spaces on the right hand side!
+// --- Examples
+//   steps(2)
+//       '# '
+//       '##'
+//   steps(3)
+//       '#  '
+//       '## '
+//       '###'
+//   steps(4)
+//       '#   '
+//       '##  '
+//       '### '
+//       '####'
+
+function steps(n, row = 0, stair = '') {
+  if (n === row) {
+    return;
+  }
+
+  if (stair.length === n) {
+    console.log(stair);
+    steps(n, row + 1);
+    return;
+  }
+
+  // if (stair.length <= row) {
+  //   stair += '#';
+  // } else {
+  //   stair += ' ';
+  // }
+  // steps(n, row, stair);
+  const add = stair.length <= row ? (stair += '#') : (stair += ' ');
+  steps(n, row, add);
+}
+steps(3);
+module.exports = steps;
+
+// function steps(n) {
+//   for (let i = 1; i <= n; i++) {
+//     let line = '';
+//     for (let x = 1; x <= n; x++) {
+//       if (x <= i) {
+//         line += '#';
+//       } else {
+//         line += ' ';
+//       }
+//     }
+//     debugger;
+//     console.log(line);
+//   }
+// }
